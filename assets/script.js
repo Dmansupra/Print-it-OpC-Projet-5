c55e79ab436000fe7419902c42aa0ca1a54c949a
David Cabot <david.cabot@laposte.net>
2024-05-07 11:03:43 +0200

    update script et html

--- assets/script.js
+++ assets/script.js
@@ -14,67 +14,53 @@ const slides = [
 	{
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+	},
+	
 ]
+
 
 let index=0
 const arrowRight=document.querySelector(".arrow_right")
 const arrowLeft=document.querySelector(".arrow_left")
 const bannerImg=document.querySelector(".banner-img")
 const text = document.querySelector("#banner p")
-const dot1 = document.getElementById('dot1')
-const dot2 = document.getElementById('dot2')
-const dot3 = document.getElementById('dot3')
-const dot4 = document.getElementById('dot4')
-const dotAll = [dot1, dot2, dot3, dot4]
+const dots=document.querySelector(".dots")
 
+const generateDots = () => {
+	dots.innerHTML = ""
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement("div")
+		if (i === index) {
+		dot.className = "dot dot_selected"
+		} else {
+			dot.className = "dot" 
+		}
+		dots.appendChild(dot)
+	}
+}
+generateDots()
+const updateData = () => {
+	bannerImg.src= "./assets/images/slideshow/"+slides[index].image
+	text.innerHTML = slides[index].tagLine
+	generateDots();
+}
 const goToNextImage=()=>{
 	index=index+1
 	if(index>slides.length-1){
 		index=0
 	}
-	console.log(index)
-	console.log(slides[index])
-	bannerImg.src= "./assets/images/slideshow/"+slides[index].image
-	text.innerHTML = slides[index].tagLine
-	changeDot();
+updateData()
 }
-setInterval(goToNextImage, 5000);
+//setInterval(goToNextImage, 5000);
 
 const goToPreviousImage=()=>{
 	index=index-1
 	if(index<0){
 		index=slides.length-1
 	}
-	console.log(index)
-	console.log(slides[index])
-	bannerImg.src= "./assets/images/slideshow/"+slides[index].image
-	text.innerHTML = slides[index].tagLine
-	changeDot();
+	updateData()
 }
 arrowRight.addEventListener("click", goToNextImage)
 arrowLeft.addEventListener("click", goToPreviousImage)
 
-
-const changeDot = () => {
-	dotAll.forEach(dotSelected => {
-		dotSelected.classList.remove('dot_selected')
-	});
-	switch (index) {
-		case 0:
-			dot1.classList.add('dot_selected')
-			break;
-		case 1:
-			dot2.classList.add('dot_selected')
-			break;
-		case 2:
-			dot3.classList.add('dot_selected')
-			break;
-		case 3:
-			dot4.classList.add('dot_selected')
-			break;
-		default:
-			break;
-	}
-}
 
